3e88dcdc22f753bed331ad3959cb3b257107346e
aasblume-chan <3giorginis@gmail.com>
2019-08-24 13:50:16 +0200

    *Assets reingeladen *Character-Prefab angepasst *Player-Dummy rausgenommen

---
+++ bittersuite/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {        
+        anim = gameObject.GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey (KeyCode.W))
+        {
+            anim.SetInteger("AnimPar", 1);
+        }
+        else
+        {
+            anim.SetInteger("AnimPar", 0);
+        }
+    }
+}
