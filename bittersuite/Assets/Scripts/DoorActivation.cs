9dd9a52c89ff495485b617ec84fef6c721cd0753
Kijubei <kijubei@gmail.com>
2020-06-03 21:36:18 +0200

    rotationanimation #1

--- bittersuite/Assets/Scripts/DoorActivation.cs
+++ bittersuite/Assets/Scripts/DoorActivation.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class DoorActivation : Activateable
 {
+    private bool isMovementNeeded = false;
+    private Quaternion rotationDestination;
+    private int moveRadius = 0;
+    public float moveSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,17 +18,51 @@ public class DoorActivation : Activateable
     // Update is called once per frame
     void Update()
     {
-        
+        if (isMovementNeeded)
+        {
+            moveDoor();
+        }
     }
     
     override public void activate() 
     {
         Debug.Log("Door Activated");
+        openDoor();
     }
 
     override public void deactivate()
     {
         Debug.Log("Door Deactivated");
+        closeDoor();
+    }
 
+    private void openDoor()
+    {
+        isMovementNeeded = true;
+
+        Quaternion startRotation = this.gameObject.transform.rotation ;
+        rotationDestination = Quaternion.Euler( new Vector3(0,100,0) ) * startRotation ;
+    }
+
+    private void closeDoor()
+    {
+        isMovementNeeded = true;
+
+        Quaternion startRotation = this.gameObject.transform.rotation ;
+        rotationDestination = Quaternion.Euler( new Vector3(0,0,0) ) * startRotation ;
+    }
+
+    private void moveDoor()
+    {
+        float delta = moveSpeed * Time.deltaTime;
+
+        Quaternion currentRotation = this.gameObject.transform.rotation;
+
+        currentRotation = Quaternion.Lerp( currentRotation, rotationDestination, delta );    
+        
+        if (currentRotation == rotationDestination)
+        {
+            isMovementNeeded = false;
+        }
     }
 }
